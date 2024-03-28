82731ce848e23a239c07d15f55871b572babbb31
Edison Cintra de Oliveira Filho <edisoncintra6@gmail.com>
2024-03-28 18:10:50 -0300

    first commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer (){
+
+    return(
+            <footer>
+                <p>&copy; {new Date().getFullYear()}Dana website</p>
+            </footer>
+    );
+}
+
+export default Footer
