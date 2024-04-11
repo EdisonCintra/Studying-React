03b7dcac3c86e68f8ab4454c6836791ea4390f05
Edison Cintra de Oliveira Filho <edisoncintra6@gmail.com>
2024-04-11 18:31:32 -0300

    React course

---
+++ react-course/src/App.jsx
@@ -0,0 +1,13 @@
+import {Todo} from "./components/Todo.jsx";
+
+export function App() {
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <Todo text="Learn React"></Todo>
+            <Todo text="Master React"></Todo>
+            <Todo text="Explore the full course React"></Todo>
+        </div>
+    )
+}
+
---
+++ react-course/src/components/Todo.jsx
@@ -0,0 +1,15 @@
+export function Todo(props) {
+
+    function deleteHandler(){
+
+    }
+
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+                <button className="btn" onClick={deleteHandler}>Delete</button>
+            </div>
+        </div>
+    )
+}
